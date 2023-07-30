8665bcfe81225270013c04c776eb0401eb1940df
Gregory Vincent <gregvjrr@gmail.com>
2023-07-30 16:55:13 -0400

    Comment change to test if Amplify works

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import HomePage from "./Homepage";
 export default function App() {
   return (
     <div className = "flex flex-1 flex-col items-center text-sky-600 bg-white">
-      <HomePage/>
+      <HomePage/> 
       <ContactForm/>
     </div>
   )
